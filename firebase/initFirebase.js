ef2de74d0df90275d2a0dda02b7e612be3b5f758
rupCS67 <rupCS67@gmail.com>
2022-03-25 18:34:55 +0300

    enable firebases

---
+++ firebase/initFirebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
+};
+
+export default function initFirebase() {
+    const app = initializeApp(firebaseConfig);
+    return getDatabase(app);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,37 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import NavBar from './src/components/NavBar'
 import MapComponent from './src/components/MapComponent'
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
+import initFirebase from '../firebase/initFirebase'
+import { getVehiclesFromDatabase, createListenersOnVehicles } from '../firebase/vehicle_functions';
 
 export default function Home() {
-  
-  const [isOpen,setIsOpen] = useState(false)
+  const db = initFirebase();
+  const [isOpen, setIsOpen] = useState(false)
+  const [vehicles, setVehicles] = useState({})
+
+  useEffect(() => {
+    (async () => {
+      try {
+        // console.log((await getVehiclesFromDatabase(db)).length);
+        await setVehicles(await getVehiclesFromDatabase(db));
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, [])
+
+  useEffect(() => {
+    (async () => {
+      for (let vehicle in vehicles) {
+        let change = await createListenersOnVehicles(db, vehicle);
+      }
+    })();
+  }, [])
 
   return (
     <div className='w-full h-screen'>
       <NavBar isOpen={isOpen} setIsOpen={setIsOpen} />
-      <MapComponent isOpen={isOpen}/>
+      <MapComponent isOpen={isOpen} />
     </div>
   )
 }
