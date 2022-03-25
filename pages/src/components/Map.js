3ba445f02738ac0491ffd931ea7180382bc81bab
danielezraa <80195225+danielezraa@users.noreply.github.com>
2022-03-25 17:33:46 +0300

    map component template is ready

--- pages/src/components/Map.js
+++ pages/src/components/Map.js
@@ -1,11 +1,135 @@
 import React from 'react'
 import Filter from './Filter'
+import GoogleMapReact from 'google-map-react';
 
-const Map = ({isOpen}) => {
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+const Map = ({ isOpen }) => {
+
     return (
-        <div className='relative w-2/3 bg-green-100 h-full'>
-            <p>here the map component</p>
-            <Filter isOpen={isOpen}/>
+        <div className='relative w-2/3 h-full'>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyB9mAs9XA7wtN9RdKMKRig7wlHBfUtjt1g' }}
+                defaultCenter={{
+                    lat: 32.07,
+                    lng: 34.80
+                }}
+                defaultZoom={11}
+                options={{
+                    styles: [
+                        {
+                            "featureType": "administrative",
+                            "elementType": "geometry",
+                            "stylers": [
+                                {
+                                    "visibility": "off"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "administrative.land_parcel",
+                            "stylers": [
+                                {
+                                    "visibility": "off"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "administrative.neighborhood",
+                            "stylers": [
+                                {
+                                    "visibility": "off"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "poi",
+                            "stylers": [
+                                {
+                                    "visibility": "off"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "poi",
+                            "elementType": "labels.text",
+                            "stylers": [
+                                {
+                                    "visibility": "off"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "road",
+                            "elementType": "labels",
+                            "stylers": [
+                                {
+                                    "visibility": "off"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "road",
+                            "elementType": "labels.icon",
+                            "stylers": [
+                                {
+                                    "visibility": "off"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "road.arterial",
+                            "elementType": "labels",
+                            "stylers": [
+                                {
+                                    "visibility": "off"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "road.highway",
+                            "elementType": "labels",
+                            "stylers": [
+                                {
+                                    "visibility": "off"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "road.local",
+                            "stylers": [
+                                {
+                                    "visibility": "off"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "transit",
+                            "stylers": [
+                                {
+                                    "visibility": "off"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "water",
+                            "elementType": "labels.text",
+                            "stylers": [
+                                {
+                                    "visibility": "off"
+                                }
+                            ]
+                        }
+                    ]
+                }}
+            >
+                   <AnyReactComponent
+            lat={32.0555684}
+            lng={34.759562}
+            text="My Marker"
+          />
+            </GoogleMapReact>
+            <Filter isOpen={isOpen} />
         </div>
     )
 }
