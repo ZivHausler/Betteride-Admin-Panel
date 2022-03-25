a6059e7df455a570c361a18e1bad0716792e7e92
danielezraa <80195225+danielezraa@users.noreply.github.com>
2022-03-25 13:25:56 +0300

    dir fix

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import NavBar from '../src/components/NavBar'
-import MapComponent from '../src/components/MapComponent'
+import NavBar from './src/components/NavBar'
+import MapComponent from './src/components/MapComponent'
 
 export default function Home() {
   return (
