7aee9e0de4c5f2c34713e52e016b26ab652e18e9
danielezraa <80195225+danielezraa@users.noreply.github.com>
2022-03-25 13:17:58 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import NavBar from '../src/components/NavBar'
+import MapComponent from '../src/components/MapComponent'
+
+export default function Home() {
+  return (
+    <div className='w-full h-screen'>
+      <NavBar/>
+      <MapComponent/>
+    </div>
+  )
+}
