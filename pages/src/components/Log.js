e194d55180cc4615ff57799a91cc5769bc8d472a
danielezraa <80195225+danielezraa@users.noreply.github.com>
2022-03-28 17:24:34 +0300

    fixing log colors and animation

--- pages/src/components/Log.js
+++ pages/src/components/Log.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
-const Log = ({ time, type, text, server, index }) => {
+const Log = ({ time, type, text, server }) => {
     return (
         <div onClick={() => alert(`time: ${time}\nserver: ${server}\ntype: ${type}\ntext: ${text}`)}
             className={`font-mono transition ease-in-out duration-100 text-xs w-[95%] bg-white pl-2 py-1 my-1 rounded-lg shadow-md cursor-pointer hover:bg-gray-200 flex items-center`}>
             <div className='w-2.5 h-2.5 rounded-full shadow-sm mr-1 '>
-                <div className={`w-2.5 h-2.5 rounded-full bg-${type === "OK" ? "green" : type === "ERROR" ? "red" : 'yellow'}-400 shadow-sm mr-1 animate-pulse`}></div>
+                {type === "ERROR" ?
+                    <span class="relative flex h-2.5 w-2.5">
+                        <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
+                        <span class="relative inline-flex rounded-full h-2.5 w-2.5 bg-red-500"></span>
+                    </span> :
+                    <div className={`w-2.5 h-2.5 rounded-full shadow-sm mr-1 ${type === "WARNING" ?  "bg-yellow-400 animate-pulse" : "bg-green-400"}`}></div>
+                }
             </div>
             <div className='mr-1'>{`[${time}]`}</div>
             <div className='pr-1'>{`${text}`}</div>
